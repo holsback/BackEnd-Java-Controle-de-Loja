635cc65be079311d45531427c6ae7c2c3e20338a
Holsback <holsback26@gmail.com>
2025-05-19 18:40:04 -0300

    Project Start

---
+++ src/main/java/org/holsback/tweetech/TweeTechApplication.java
@@ -0,0 +1,13 @@
+package org.holsback.tweetech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweeTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TweeTechApplication.class, args);
+    }
+
+}
